6e97ac83d72bf1e59175a9b207ca56c722121004
lweiheng <lweiheng@outlook.com>
2018-12-28 20:50:53 -0500

    metasploit-framework

---
+++ metasploit-framework.sh
@@ -0,0 +1,5 @@
+ 
+apt-get install curl,wget
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+  chmod 755 msfinstall && \
+  ./msfinstall
